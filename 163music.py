8c24d37f2956e0fc58075b3f17cfd2d13761887e
NOISE <46085246+rcy1314@users.noreply.github.com>
2023-02-04 21:18:59 +0800

    1

--- 163music.py
+++ 163music.py
@@ -30,10 +30,10 @@ def protect(text):
     return {'params':encrypt('TA3YiYCfY2dDJQgg',encrypt('0CoJUm6Qyw8W8jud',text)),'encSecKey':'84ca47bca10bad09a6b04c5c927ef077d9b9f1e37098aa3eac6ea70eb59df0aa28b691b7e75e4f1f9831754919ea784c8f74fbfadf2898b0be17849fd656060162857830e241aba44991601f137624094c114ea8d17bce815b0cd4e5b8e2fbaba978c6d1d14dc3d1faf852bdd28818031ccdaaa13a6018e1024e2aae98844210'}
 
 def getSongData(uid, song_type = 1):
-    url = 'https://music.163.com/weapi/v1/play/record?csrf_token='
+    url = 'https://music.163.com/weapi/v1/play/record?csrf_token=710f0cd35eccd738ee165817f0ae81a79246fec3fb91f2a381b8ab853b2a61fc519e07624a9f0053f538629659aaef3a02566bb05041e2c583609b113325f19a54f9df9402492d99a0d2166338885bd7'
     data = {
         'uid': 105915001, #用户uid 可以到网页版搜索下用户进入到主页获取
-        'type': 0, #1最近一周 0所有时间
+        'type': 1, #1最近一周 0所有时间
     }
     session = requests.session()
     res=session.post(url,protect(json.dumps(data)),headers=headers)
